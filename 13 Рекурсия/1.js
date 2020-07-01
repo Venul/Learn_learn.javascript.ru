388f03a50609caad068c3cb1dbbaede45979bab3
Lunev Sergey <s.lunev88@gmail.com>
2020-07-01 08:29:34 +0300

    add recursion 1,2

---
+++ 13 Рекурсия/1.js
@@ -0,0 +1,20 @@
+function sum(n) {
+  let res = 0;
+  for (let i=0; i<=n; i++) {
+    res +=i;
+  }
+  return res;
+}
+
+function sum2(n) {
+  return n==1 ? n : sum2(n-1)+n;
+}
+
+function sum3(n) {
+ return (n*(n+1))/2;
+}
+
+
+console.log(sum(10000));
+console.log(sum2(10000));
+console.log(sum3(10000));
---
+++ 13 Рекурсия/2.js
@@ -0,0 +1,6 @@
+function f(n) {
+  return n==1 ? n : f(n-1)*n;
+  // return n ? n * factorial(n - 1) : 1;
+}
+
+console.log(f(1))
